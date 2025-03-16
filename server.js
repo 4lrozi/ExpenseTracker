7daf2a8420b62671c9fe14531c6f5bf5051c58de
4lrozi <fathur.rozi.alfitrah@gmail.com>
2025-03-16 04:35:00 +0000

    add ip

--- server.js
+++ server.js
@@ -1,4 +1,11 @@
 const app = require('./src/app');
 const { port } = require('./src/config/env');
 
+const publicIp = require('public-ip');
+
+(async () => {
+  console.log("Public IPv4:", await publicIp.publicIpv4());
+  console.log("Public IPv6:", await publicIp.publicIpv6());
+})();
+
 app.listen(port, () => console.log(`🚀 Server running on port ${port}`));
