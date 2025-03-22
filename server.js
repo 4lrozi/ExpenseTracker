abbca224e314e912ef1cd5e91fc233be444ce677
4lrozi <fathur.rozi.alfitrah@gmail.com>
2025-03-22 06:42:35 +0000

    delete ip

--- server.js
+++ server.js
@@ -1,11 +1,4 @@
 const app = require('./src/app');
 const { port } = require('./src/config/env');
 
-const publicIp = require('public-ip');
-
-(async () => {
-  console.log("Public IPv4:", await publicIp.publicIpv4());
-  console.log("Public IPv6:", await publicIp.publicIpv6());
-})();
-
 app.listen(port, () => console.log(`🚀 Server running on port ${port}`));
